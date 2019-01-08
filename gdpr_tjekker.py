a848edcba6d33d16d6757386fda975630d3cbb63
danielarnason <danielarnason85@gmail.com>
2019-01-08 11:30:01 +0100

    GdprTjekker class lavet

--- gdpr_tjekker.py
+++ gdpr_tjekker.py
@@ -1,19 +1,19 @@
-from os import walk, remove
-from os.path import join
-import logging
+from pathlib import Path
 
-logging.basicConfig(level=logging.DEBUG, filename='log.log')
+class GdprTjekker:
 
-filer = []
+    def __init__(self, path):
+        self.p = Path(path)
 
-for dirpath, dirnames, filenames in walk(r'Q:\CPK\Byggesag\ejendomme'):
-    for file in filenames:
-        if file.endswith('.xls') or file.endswith('.xlsx'):
-            filepath = join(dirpath, file)
-            try:
-                remove(filepath)
-                print(f'{filepath} SLETTET')
-                logging.info(f'{filepath} slettet')
-            except PermissionError:
-                print(f'PERMISSIONERROR i {filepath}')
-                logging.debug(f'PermissionError i {filepath}')
+    def get_filepaths(self, extension, search_string):
+        results = []
+        all_files = list(self.p.rglob(f'*.{extension}')) 
+
+        for file in all_files:
+            if search_string in file.name.lower():
+                results.append(file)
+        
+        return results
+
+    def delete_files(self, filepath):
+        filepath.unlink()
