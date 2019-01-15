ae04ba5bc80f4ebda692408bb5bbbc84325a0ba2
danielarnason <danielarnason85@gmail.com>
2019-01-15 14:37:50 +0100

    Kan tjekke efter cpr kolonne

--- gdpr_tjekker.py
+++ gdpr_tjekker.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import pandas as pd
+import csv
 
 class GdprTjekker:
 
@@ -14,6 +16,33 @@ class GdprTjekker:
                 results.append(file)
         
         return results
+
+    def tjek_cpr(self, filepath, encoding='utf-8'):
+        if Path(filepath).suffix == '.csv':
+            try:
+                df = pd.read_csv(filepath, encoding=encoding, sep=None, engine='python')
+                cpr_file = False
+                for col in df.columns:
+                    if self.tjek_column_name(col):
+                        cpr_file = True
+                        break
+                return cpr_file
+            except (pd.errors.ParserError, csv.Error) as e:
+                return e
+        elif Path(filepath).suffix == '.xlsx':
+            df = pd.read_excel(filepath)
+            cpr_file = False
+            for col in df.columns:
+                if self.tjek_column_name(col):
+                    cpr_file = True
+                    break
+            return cpr_file
+
+    def tjek_column_name(self, col_name):
+        if 'cpr' in str(col_name).lower():
+            return True
+        else:
+            return False
 
     def delete_files(self, filepath):
         filepath.unlink()
