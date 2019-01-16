2515b868f07ae2c35ed857a751d90d241aab17ae
danielarnason <danielarnason85@gmail.com>
2019-01-16 14:51:06 +0100

    example taget med

---
+++ example.py
@@ -0,0 +1,20 @@
+from gdpr_tjekker import GdprTjekker
+import pandas as pd
+from tqdm import tqdm
+
+tjekker = GdprTjekker(XXXXXXXXX)
+
+filformater = ['xlsx', 'csv']
+
+filer = {key: None for key in filformater}
+
+for filformat in filformater:
+    cpr_filer = []
+    files = tjekker.get_filepaths(filformat, '')
+    for file in tqdm(files):
+        if tjekker.tjek_cpr(file, encoding='latin1'):
+            cpr_filer.append(file)
+    filer[filformat] = pd.Series(cpr_filer)
+
+df = pd.DataFrame.from_dict(filer)
+df.to_excel(f'{tjekker.p}\\GDPR_TJEK.xlsx', index=False)
