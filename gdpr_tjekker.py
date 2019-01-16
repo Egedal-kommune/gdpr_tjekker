6ff118ea834079e316ca3142224a7332090103e9
danielarnason <danielarnason85@gmail.com>
2019-01-16 15:04:33 +0100

    Docstrings tilføjet

--- gdpr_tjekker.py
+++ gdpr_tjekker.py
@@ -4,11 +4,33 @@ import csv
 import xlrd
 
 class GdprTjekker:
+    """
+    Parameters
+    ----------
+    path : str
+        Stien til den folder, som skal kigges igennem
+    
+    """
 
     def __init__(self, path):
         self.p = Path(path)
 
-    def get_filepaths(self, extension, search_string):
+    def get_filepaths(self, extension, search_string=''):
+        """Henter alle filer, der passer til søgekriterierne
+
+        Parameters
+        ----------
+        extension : str
+            Hvilken filekstension skal der kigges efter? csv eller xlsx?
+        
+        search_string : str, optional
+            Hvad skal filnavnene indeholde? (Default er ingenting, dvs. den søger efter alle filer)
+        
+        Returns
+        -------
+        list
+            En liste med alle de filer, der er fundet på den angivne sti
+        """
         results = []
         all_files = list(self.p.rglob(f'*.{extension}')) 
 
@@ -19,6 +41,21 @@ class GdprTjekker:
         return results
 
     def tjek_cpr(self, filepath, encoding='utf-8'):
+        """Tjekker om der er en kolonne, som hedder noget med 'cpr'.
+
+        Parameters
+        ----------
+        filepath : str
+            Stien til den fil, som skal kigges i
+        
+        encoding : str
+            Den encoding, som filerne har (Default er 'utf-8')
+        
+        Returns
+        -------
+        bool
+            True hvis filen indeholder en kolonne, som hedder noget med 'cpr', ellers False
+        """
         if Path(filepath).suffix == '.csv' and Path(filepath).name.startswith('~') == False:
             try:
                 df = pd.read_csv(filepath, encoding=encoding, sep=None, engine='python')
@@ -43,10 +80,29 @@ class GdprTjekker:
             return cpr_file
 
     def tjek_column_name(self, col_name):
+        """Tjekker om navnet på en kolonne indeholder 'cpr'
+
+        Parameters
+        ----------
+        col_name : str
+            Navnet på den kolonne, som skal tjekkes efter
+
+        Returns
+        -------
+        bool
+            True hvis kolonnenavn har 'cpr' i ellers False
+        """
         if 'cpr' in str(col_name).lower():
             return True
         else:
             return False
 
     def delete_files(self, filepath):
+        """Sletter en given fil
+
+        Parameters
+        ----------
+        filepath : str
+            Sti til den fil, der skal slettes
+        """
         filepath.unlink()
